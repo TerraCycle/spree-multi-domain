04cd713eda1d450ff1d67f45e51cc0923223fb8d
Kris Birov <kris.birov@gmail.com>
2021-11-09 19:40:40 +0200

    Move decorators to the namespaced folder

---
+++ app/models/spree_multi_domain/product_decorator.rb
@@ -0,0 +1,12 @@
+module SpreeMultiDomain
+  module ProductDecorator
+    def self.prepended(base)
+      base.has_many :store_products, class_name: '::Spree::StoreProduct', dependent: :destroy
+      base.has_many :stores, through: :store_products, class_name: '::Spree::Store'
+
+      base.scope :by_store, ->(store) { joins(:stores).where(spree_products_stores: { store_id: store.id }) }
+    end
+  end
+end
+
+::Spree::Product.prepend(SpreeMultiDomain::ProductDecorator)
--- app/models/spree_multi_domain/taxon_decorator.rb
+++ app/models/spree_multi_domain/taxon_decorator.rb
@@ -1,7 +1,7 @@
-module Spree
+module SpreeMultiDomain
   module TaxonDecorator
     def self.prepended(base)
-      base.scope :by_store, ->(store_id) { joins(:taxonomy).merge(Spree::Taxonomy.by_store(store_id)) }
+      base.scope :by_store, ->(store_id) { joins(:taxonomy).merge(::Spree::Taxonomy.by_store(store_id)) }
       base.extend ClassMethods
     end
 
@@ -13,4 +13,4 @@ module Spree
   end
 end
 
-::Spree::Taxon.prepend(Spree::TaxonDecorator)
+::Spree::Taxon.prepend(SpreeMultiDomain::TaxonDecorator)
