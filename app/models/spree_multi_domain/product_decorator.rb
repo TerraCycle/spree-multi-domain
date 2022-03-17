79d00db0661878b6786fcc5eb02865ace5453558
Kris Birov <kris.birov@gmail.com>
2022-03-17 06:33:48 +0100

    Set searcher_class

--- app/models/spree_multi_domain/product_decorator.rb
+++ app/models/spree_multi_domain/product_decorator.rb
@@ -4,7 +4,7 @@ module SpreeMultiDomain
       base.has_many :store_products, class_name: '::Spree::StoreProduct', dependent: :destroy
       base.has_many :stores, through: :store_products, class_name: '::Spree::Store'
 
-      base.scope :by_store, ->(store) { joins(:stores).where(spree_products_stores: { store_id: store.id }) }
+      base.scope :by_store, ->(store_id) { joins(:stores).where(spree_products_stores: { store_id: store_id }) }
     end
   end
 end
